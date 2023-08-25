1cee5be11dd01345036accc802fa12af384bc397
Tymoniden <timo.steib@gmail.com>
2023-08-25 23:07:49 +0200

    Change project from WASM to server

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace Fridgelight.Shared
+namespace Fridgelight.Data
 {
     public class WeatherForecast
     {
@@ -6,8 +6,8 @@ namespace Fridgelight.Shared
 
         public int TemperatureC { get; set; }
 
-        public string? Summary { get; set; }
-
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
     }
 }
